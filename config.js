ad9269b3bae98da2c8b71f996a113e55b4815ab5
Vat-9 <vatvar73@gmail.com>
2024-11-21 13:35:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "58xFlQKK#SsgGzEMkINqPtc8V9SmGAZU5CEam-2w4tjv1xUQV0SY",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/t4kl7i.jpeg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hellow......i am vat-9 wa bot alive now ✅💀",
 };
